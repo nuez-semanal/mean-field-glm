1a66e54cf91fa566bca0d197e15c4f992e9af72a
nuez-semanal <saenz.manuel@gmail.com>
2024-11-28 16:58:55 +0000

    Minor changes to simulation scripts.

--- simulation-scripts/order_parameters_vs_snr.py
+++ simulation-scripts/order_parameters_vs_snr.py
@@ -3,18 +3,21 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from mean_field_model.block_computation import BlockComputation
+import mean_field_model as mf
 
-block_arguments = {"variable" : "snr",
-                   "fixed_var" : 1.0,
-                   "num_per_var" : 10,
-                   "var_list" : [0.1, 1.0, 10.0, 100.0, 1000.0],
-                   "prior" : "Normal",
-                   "signal" : "Normal",
-                   "log_likelihood" : "Logistic",
-                   "file_name" : "order_parameters_vs_snr",
-                   "bayes_optimal" : True}
+def main():
+    block_arguments = {"variable": "snr",
+                       "fixed_var": 1.0,
+                       "num_per_var": 10,
+                       "var_list": [0.1, 1.0, 10.0, 100.0, 1000.0],
+                       "prior": "Normal",
+                       "signal": "Normal",
+                       "log_likelihood": "Logistic",
+                       "file_name": "order_parameters_vs_snr",
+                       "bayes_optimal": True}
 
-block = BlockComputation(**block_arguments)
+    block = mf.block_computation.BlockComputation(**block_arguments)
+    block.compute_data()
 
-block.compute_data()
+if __name__ == "__main__":
+    main()
