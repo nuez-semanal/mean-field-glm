066b7e0a3f18cef186d48728408804c7248dedbc
nuez-semanal <saenz.manuel@gmail.com>
2025-06-19 20:05:24 -0300

    New simulations added

--- simulation-scripts/compute_order_parameters_vs_kappa_beta.py
+++ simulation-scripts/compute_order_parameters_vs_kappa_beta.py
@@ -9,9 +9,9 @@ from mean_field_glm import block_beta_computation
 
 def main():
     block_arguments = {"variable": "kappa",
-                       "fixed_var": 100.0,
+                       "fixed_var": 1.0,
                        "num_per_var": 20,
-                       "var_tuple": (0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,3.0),
+                       "var_tuple": (0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5),
                        "file_name": "order_parameters_vs_kappa_beta",
                        "bayes_optimal": False}
 
--- simulation-scripts/compute_order_parameters_vs_snr_gaussian.py
+++ simulation-scripts/compute_order_parameters_vs_snr_gaussian.py
@@ -7,11 +7,10 @@ import mean_field_glm as mf
 
 def main():
     block_arguments = {"variable": "snr",
-                       "fixed_var": 1.0,
-                       "num_per_var": 10,
-                       "var_tuple": (0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0,
-                                     3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5,
-                                     7.0, 8.0, 9.0, 10.0, 15.0),
+                       "fixed_var": 0.25,
+                       "num_per_var": 5,
+                       "var_tuple": (3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5,
+                                     7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0),
                        "signal": "Normal",
                        "log_likelihood": "Logistic",
                        "file_name": "order_parameters_vs_snr_gaussian",
